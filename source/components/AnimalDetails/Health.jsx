927f30c1d091a6bca197a127cd4cf528b02fa3a8
Samir Zaman <samir.zaman40@gmail.com>
2025-11-07 17:20:23 -0800

    health SVG's on foster animal page are not showing up on deployed site. importing SVGs at the top

--- source/components/AnimalDetails/Health.jsx
+++ source/components/AnimalDetails/Health.jsx
@@ -1,6 +1,45 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
 
+import vaccineIcon from '../../assets/icons/Health/vaccine.svg'
+import surgeryIcon from '../../assets/icons/Health/surgery.svg'
+import barkIcon from '../../assets/icons/Health/bark.svg'
+import toiletIcon from '../../assets/icons/Health/toilet.svg'
+import otherIcon from '../../assets/icons/Health/other.svg'
+
+export default function Health() {
+    const { animal } = useOutletContext()
+
+    return (
+        <>
+            <div className="health-stat-container">
+                <img className="icon" src={vaccineIcon} alt="Syringe icon" />
+                <p>Vaccinations: {animal.vaccines}</p>
+            </div>
+            <div className="health-stat-container">
+                <img className="icon" src={surgeryIcon} alt="Surgery icon" />
+                <p>Spayed/neutered: {animal.reproductive}</p>
+            </div>
+            <div className="health-stat-container">
+                <img className="icon" src={barkIcon} alt="Obedience icon" />
+                <p>Obedience training: {animal.behavior}</p>
+            </div>
+            <div className="health-stat-container">
+                <img className="icon" src={toiletIcon} alt="House training icon" />
+                <p>House training: {animal.houseTrained}</p>
+            </div>
+            <div className="health-stat-container">
+                <img className="icon" src={otherIcon} alt="Other icon" />
+                <p>Other: {animal.other}</p>
+            </div>
+        </>
+    )
+}
+
+
+/*import React from 'react'
+import { useOutletContext } from 'react-router-dom'
+
 export default function Health() {
     const{animal} = useOutletContext()
     return(
@@ -28,6 +67,7 @@ export default function Health() {
         </>
     )
 }
+*/
 
 /*
 aggression
